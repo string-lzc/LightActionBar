3af5208185af8e08b2421e133b8e6048c9c28645
刘志朝 <lzc@Saruc.local>
2017-07-24 20:10:41 +0800

    add class DataHelper

---
+++ LightActionBar/app/src/main/java/lzc/com/date/DateHelper.java
@@ -0,0 +1,25 @@
+package lzc.com.date;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by lzc on 2017/7/24.
+ */
+
+public class DateHelper {
+    private static Date date;
+    private static SimpleDateFormat sdf;
+
+
+    public static String formatDate(String formate){
+        date = new Date();
+        sdf = new SimpleDateFormat(formate);
+        return sdf.format(date);
+    }
+    public String formateDate(){
+        date = new Date();
+        sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(date);
+    }
+}
